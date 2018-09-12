adbfd4e154b7a88bfd2f5c10b550fecd9dd8a024
xzhang <xzhang@mo9.com>
2018-09-12 20:15:15 +0800

    commit

--- src/main/java/com/mo9/raptor/engine/entity/PayOrderEntity.java
+++ src/main/java/com/mo9/raptor/engine/entity/PayOrderEntity.java
@@ -14,52 +14,74 @@ import java.math.BigDecimal;
 @Table(name = "t_raptor_pay_order")
 public class PayOrderEntity extends AbstractOrderEntity {
 
-    @Column(name = "batch_id")
-    private String batchId;
-
-    /** 支付币种 */
+    /**
+     * 支付币种
+     */
     @Column(name = "pay_currency")
     private String payCurrency;
 
-    /** 请求支付数量 */
+    /**
+     * 还款订单
+     */
+    @Column(name = "loan_order_id")
+    private String loanOrderId;
+
+    /**
+     * 请求支付数量
+     */
     @Column(name = "apply_number")
     private BigDecimal applyNumber;
 
-    /** 实际支付数量 */
+    /**
+     * 实际支付数量
+     */
     @Column(name = "pay_number")
     private BigDecimal payNumber = BigDecimal.ZERO;
 
-    /** 确认时间 */
-    @Column(name = "confirm_time")
-    private Long confirmTime = -1L;
-
-    /** 锚定币种 */
-    @Column(name = "anchor_currency")
-    private String anchorCurrency;
-
-    /** 兑换率 */
-    @Column(name = "exchange_rate")
-    private BigDecimal exchangeRate;
-
-    /** 按锚定币种换算后数量 */
-    @Column(name = "anchor_number")
-    private BigDecimal anchorNumber;
-
-    /** 实际入账数量（按锚定币种计数） */
+    /**
+     * 实际入账数量
+     */
     @Column(name = "entry_number")
     private BigDecimal entryNumber = BigDecimal.ZERO;
 
-    /** 支付时间 */
+    /**
+     * 还的本金
+     */
+    @Column(name = "paid_principal")
+    private BigDecimal paidPrincipal = BigDecimal.ZERO;
+    /**
+     * 还的利息
+     */
+    @Column(name = "paid_interest")
+    private BigDecimal paidInterest = BigDecimal.ZERO;
+    /**
+     * 还的罚息
+     */
+    @Column(name = "paid_penalty")
+    private BigDecimal paidPenalty = BigDecimal.ZERO;
+    /**
+     * 还的服务费
+     */
+    @Column(name = "paid_fee")
+    private BigDecimal paidFee = BigDecimal.ZERO;
+
+    /**
+     * 支付时间
+     */
     @Column(name = "pay_time")
     private long payTime = -1L;
 
-    /** 入账时间 */
+    /**
+     * 入账时间
+     */
     @Column(name = "entry_over_time")
     private long entryOverTime = -1L;
 
-    /** 还款订单 */
-    @Column(name = "loan_order_id")
-    private String loanOrderId;
+    /**
+     * 还除本金之外的所有金额时, 可推迟还款时间, 一般为一个账期
+     */
+    @Column(name = "postpone_days")
+    private Integer postponeDays = 0;
 
     public String getPayCurrency() {
         return payCurrency;
@@ -83,38 +105,6 @@ public class PayOrderEntity extends AbstractOrderEntity {
 
     public void setPayNumber(BigDecimal payNumber) {
         this.payNumber = payNumber;
-    }
-
-    public Long getConfirmTime() {
-        return confirmTime;
-    }
-
-    public void setConfirmTime(Long confirmTime) {
-        this.confirmTime = confirmTime;
-    }
-
-    public String getAnchorCurrency() {
-        return anchorCurrency;
-    }
-
-    public void setAnchorCurrency(String anchorCurrency) {
-        this.anchorCurrency = anchorCurrency;
-    }
-
-    public BigDecimal getExchangeRate() {
-        return exchangeRate;
-    }
-
-    public void setExchangeRate(BigDecimal exchangeRate) {
-        this.exchangeRate = exchangeRate;
-    }
-
-    public BigDecimal getAnchorNumber() {
-        return anchorNumber;
-    }
-
-    public void setAnchorNumber(BigDecimal anchorNumber) {
-        this.anchorNumber = anchorNumber;
     }
 
     public BigDecimal getEntryNumber() {
@@ -141,19 +131,51 @@ public class PayOrderEntity extends AbstractOrderEntity {
         this.entryOverTime = entryOverTime;
     }
 
-    public String getBatchId() {
-        return batchId;
-    }
-
-    public void setBatchId(String batchId) {
-        this.batchId = batchId;
-    }
-
     public String getLoanOrderId() {
         return loanOrderId;
     }
 
     public void setLoanOrderId(String loanOrderId) {
         this.loanOrderId = loanOrderId;
+    }
+
+    public BigDecimal getPaidPrincipal() {
+        return paidPrincipal;
+    }
+
+    public void setPaidPrincipal(BigDecimal paidPrincipal) {
+        this.paidPrincipal = paidPrincipal;
+    }
+
+    public BigDecimal getPaidInterest() {
+        return paidInterest;
+    }
+
+    public void setPaidInterest(BigDecimal paidInterest) {
+        this.paidInterest = paidInterest;
+    }
+
+    public BigDecimal getPaidPenalty() {
+        return paidPenalty;
+    }
+
+    public void setPaidPenalty(BigDecimal paidPenalty) {
+        this.paidPenalty = paidPenalty;
+    }
+
+    public BigDecimal getPaidFee() {
+        return paidFee;
+    }
+
+    public void setPaidFee(BigDecimal paidFee) {
+        this.paidFee = paidFee;
+    }
+
+    public Integer getPostponeDays() {
+        return postponeDays;
+    }
+
+    public void setPostponeDays(Integer postponeDays) {
+        this.postponeDays = postponeDays;
     }
 }
