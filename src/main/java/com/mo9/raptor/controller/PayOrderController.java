9b8d32819996a782d8ec05c71c6aea3921dfb912
xzhang <xzhang@mo9.com>
2018-09-13 19:00:23 +0800

    commit

--- src/main/java/com/mo9/raptor/controller/PayOrderController.java
+++ src/main/java/com/mo9/raptor/controller/PayOrderController.java
@@ -4,12 +4,18 @@ import com.alibaba.fastjson.JSONObject;
 import com.mo9.raptor.bean.BaseResponse;
 import com.mo9.raptor.bean.req.CashRenewalReq;
 import com.mo9.raptor.bean.req.CashRepayReq;
+import com.mo9.raptor.bean.res.ChannelDetailRes;
+import com.mo9.raptor.enums.RepayChannelTypeEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 还款
@@ -20,29 +26,48 @@ public class PayOrderController {
 
     private static final Logger logger = LoggerFactory.getLogger(PayOrderController.class);
 
-
     /**
-     * 还清
+     * TODO:还清
      * @param req
      * @return
      */
-    @PostMapping("repay")
-    public BaseResponse<JSONObject> repay(@Valid CashRepayReq req) {
+    @PostMapping("/repay")
+    public BaseResponse<JSONObject> repay(@Valid @RequestBody CashRepayReq req) {
         BaseResponse<JSONObject> response = new BaseResponse<JSONObject>();
 
         return response;
     }
 
     /**
-     * 续期
+     * TODO: 续期
      * @param req
      * @return
      */
-    @PostMapping("renewal")
-    public BaseResponse<JSONObject> renewal(@Valid CashRenewalReq req) {
+    @PostMapping("/renewal")
+    public BaseResponse<JSONObject> renewal(@Valid @RequestBody CashRenewalReq req) {
         BaseResponse<JSONObject> response = new BaseResponse<JSONObject>();
 
         return response;
+    }
+
+    /**
+     * 获取渠道列表
+     * @return
+     */
+    @GetMapping("/get_repay_channels")
+    public BaseResponse<JSONObject> getRepayChannels () {
+        BaseResponse<JSONObject> response = new BaseResponse<JSONObject>();
+        List<ChannelDetailRes> channels = new ArrayList<ChannelDetailRes>();
+        for (RepayChannelTypeEnum channelType : RepayChannelTypeEnum.values()) {
+            ChannelDetailRes res = new ChannelDetailRes();
+            res.setChannelName(channelType.getChannelName());
+            res.setChannelType(channelType.getChannelType());
+            res.setUseType(channelType.getChannelUseType().getDesc());
+            channels.add(res);
+        }
+        JSONObject data = new JSONObject();
+        data.put("entities", channels);
+        return response.buildSuccessResponse(data);
     }
 
 }
--- src/main/java/com/mo9/raptor/enums/RepayChannelTypeEnum.java
+++ src/main/java/com/mo9/raptor/enums/RepayChannelTypeEnum.java
@@ -5,4 +5,43 @@ package com.mo9.raptor.enums;
  * Created by xzhang on 2018/9/13.
  */
 public enum RepayChannelTypeEnum {
+
+    /**
+     * 易联H5
+     */
+    YILIANPAY_H5(1, ChannelUseType.LINK, "易联H5支付"),
+    ;
+
+    /**
+     * 渠道名
+     */
+    private String channelName;
+
+    /**
+     * 渠道编号
+     */
+    private Integer channelType;
+
+    /**
+     * 渠道使用方式
+     */
+    private ChannelUseType channelUseType;
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public Integer getChannelType() {
+        return channelType;
+    }
+
+    public ChannelUseType getChannelUseType() {
+        return channelUseType;
+    }
+
+    RepayChannelTypeEnum(Integer channelType, ChannelUseType channelUseType, String channelName) {
+        this.channelName = channelName;
+        this.channelType = channelType;
+        this.channelUseType = channelUseType;
+    }
 }
