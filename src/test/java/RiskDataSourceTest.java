0574ef9b0381878d202ccae186c85c672ace49f6
weiwt <felix@21>
2018-09-17 15:01:13 +0800

    multi datasource test

---
+++ src/test/java/RiskDataSourceTest.java
@@ -0,0 +1,39 @@
+import com.mo9.raptor.risk.entity.TRiskCallLog;
+import com.mo9.raptor.risk.repo.RiskCallLogRepository;
+import com.mo9.raptor.risk.service.RiskCallLogService;
+import org.junit.Test;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author wtwei .
+ * @date 2018/9/17 .
+ * @time 10:15 .
+ */
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class RiskDataSourceTest extends BaseTest{
+    
+    @Resource
+    private RiskCallLogService riskCallLogService;
+    
+    
+    @Test
+    public void save(){
+        for (int i = 0; i < 1000; i++) {
+            TRiskCallLog callLog = new TRiskCallLog();
+            callLog.setMobile("1590000100" + i);
+            callLog.setCallTel("10010");
+            riskCallLogService.save(callLog);
+        }
+        
+    }
+    
+    @Test
+    public void saveTelInfo(){
+        
+    }
+}
