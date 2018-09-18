ff9b66b8bdead193580a699ea8cacc30a7c4e194
weiwt <felix@21>
2018-09-18 16:59:33 +0800

    call log json write to local file

--- src/main/java/com/mo9/raptor/controller/RiskController.java
+++ src/main/java/com/mo9/raptor/controller/RiskController.java
@@ -18,7 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.SimpleFormatter;
 
 /**
  * @author wtwei .
@@ -29,6 +36,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/risk")
 public class RiskController {
+    private static final String FILE_PATH = "/data/calllogfile/";
+    
     private static Logger logger = LoggerFactory.getLogger(RiskController.class);
     
     @Resource
@@ -60,9 +69,36 @@ public class RiskController {
         List<TRiskCallLog> riskCallLogList = riskCallLogService.coverReqToEntity(callLogReq);
         riskCallLogService.batchSave(riskCallLogList);
             
+        writeFile2Disk(callLogReq.toString(), callLogReq.getData().getTel());
         //todo 保存完成后，数据传给忆楠一份
         
         
         return "ok";
+    }
+    
+    private void writeFile2Disk(String str, String mobile){
+        Date date = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+        String dirName = sf.format(date);
+        String dir = FILE_PATH + dirName;
+
+        File dirF = new File(dir);
+        if (!dirF.exists()){
+            dirF.mkdir();
+        }
+        
+        try {
+            File file = new File(dirF + File.separator + mobile + ".json");
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(str.getBytes());
+            
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
