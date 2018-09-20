c2a355f0903ede6df822c867c511003f3644f3be
guxt <xtgu@mo9.com>
2018-09-20 16:03:47 +0800

    银行卡增加日志表 , 增加五分钟四次验证限制

---
+++ src/main/java/com/mo9/raptor/utils/upload/CommonUtils.java
@@ -0,0 +1,19 @@
+package com.mo9.raptor.utils.upload;
+
+import com.mo9.raptor.redis.RedisServiceApi;
+import com.mo9.raptor.utils.CommonValues;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by xtgu on 2018/9/20.
+ * @author xtgu
+ */
+
+public class CommonUtils {
+
+}
