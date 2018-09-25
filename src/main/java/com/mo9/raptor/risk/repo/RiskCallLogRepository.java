3dd43108cc47506f2c1342d7c39c8043603a9acb
weiwt <felix@21>
2018-09-25 17:37:18 +0800

    add find function

--- src/main/java/com/mo9/raptor/risk/repo/RiskCallLogRepository.java
+++ src/main/java/com/mo9/raptor/risk/repo/RiskCallLogRepository.java
@@ -1,6 +1,7 @@
 package com.mo9.raptor.risk.repo;
 
 import com.mo9.raptor.risk.entity.TRiskCallLog;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,8 +16,10 @@ public interface RiskCallLogRepository extends JpaRepository<TRiskCallLog, Long>
 
     @Query(value = "select * from t_risk_call_log where uid = ?1", nativeQuery = true)
     List<TRiskCallLog> getCallLogByUid(String uid);
-    
-    
+
+    @Query(value = "select * from t_risk_call_log where mobile = ?1", nativeQuery = true)
+    List<TRiskCallLog> findCallLogsByMobile(String mobile);
+
     @Query(value = "select * from t_risk_call_log where mobile = ?1 and call_to = ?2 and call_time = ?3 and deleted = false limit 1", nativeQuery = true)
     TRiskCallLog findOneCallLog(String mobile, String callTo, String callTime);
 }
