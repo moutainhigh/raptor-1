3962c95a6105d8d6c776f8cd838ba65d8fa513a2
xzhang <xzhang@mo9.com>
2018-09-28 16:20:25 +0800

    优惠券基础

---
+++ src/main/java/com/mo9/raptor/engine/service/impl/CouponServiceImpl.java
@@ -0,0 +1,28 @@
+package com.mo9.raptor.engine.service.impl;
+
+import com.mo9.raptor.engine.entity.CouponEntity;
+import com.mo9.raptor.engine.repository.CouponRespository;
+import com.mo9.raptor.engine.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 优惠券service
+ * Created by xzhang on 2018/9/28.
+ */
+@Service("couponServiceImpl")
+public class CouponServiceImpl implements CouponService {
+
+    @Autowired
+    private CouponRespository couponRespository;
+
+    @Override
+    public CouponEntity getUserUnboundCoupon(String userCode) {
+        return couponRespository.getUserUnboundCoupon(userCode);
+    }
+
+    @Override
+    public CouponEntity getByCouponId(String couponId) {
+        return couponRespository.getByCouponId(couponId);
+    }
+}
