9881249215765a55e904effdf5f559fee8a14c54
guxt <xtgu@mo9.com>
2018-09-28 19:29:13 +0800

    银行卡认证失败两次默认成功

--- src/main/java/com/mo9/raptor/repository/BankLogRepository.java
+++ src/main/java/com/mo9/raptor/repository/BankLogRepository.java
@@ -2,6 +2,9 @@ package com.mo9.raptor.repository;
 
 import com.mo9.raptor.entity.BankLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by xtgu on 2018/9/20.
@@ -10,5 +13,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BankLogRepository extends JpaRepository<BankLogEntity,Long> {
 
 
-
+    /**
+     * 查询银行卡错误日志
+     * @param mobile
+     * @param bankNo
+     * @param cardId
+     * @param userName
+     * @param status
+     * @return
+     */
+    @Query(value = "select t from BankLogEntity t where t.mobile = ?1 and t.bankNo = ?2 and t.cardId = ?3 and t.userName = ?4 and t.status = ?5 ")
+    List<BankLogEntity> findByMobileAndBankNoAndIdCardAndUserNameAndStatus(String mobile, String bankNo, String cardId, String userName, String status);
 }
