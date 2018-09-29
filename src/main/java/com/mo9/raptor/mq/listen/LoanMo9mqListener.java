34ee401e8d8eb3dfefc844001dfbc33cb9c58c20
xzhang <xzhang@mo9.com>
2018-09-29 15:01:10 +0800

    放款mq接收完善

--- src/main/java/com/mo9/raptor/mq/listen/LoanMo9mqListener.java
+++ src/main/java/com/mo9/raptor/mq/listen/LoanMo9mqListener.java
@@ -21,6 +21,7 @@ import com.mo9.raptor.engine.service.ILoanOrderService;
 import com.mo9.raptor.engine.service.IPayOrderDetailService;
 import com.mo9.raptor.engine.service.IPayOrderService;
 import com.mo9.raptor.engine.state.event.impl.lend.LendResponseEvent;
+import com.mo9.raptor.engine.state.event.impl.loan.LoanResponseEvent;
 import com.mo9.raptor.engine.state.event.impl.pay.DeductResponseEvent;
 import com.mo9.raptor.engine.state.launcher.IEventLauncher;
 import com.mo9.raptor.engine.structure.field.Field;
@@ -85,6 +86,9 @@ public class LoanMo9mqListener implements IMqMsgListener{
 
 	@Autowired
 	private IEventLauncher lendEventLauncher;
+
+    @Autowired
+    private IEventLauncher loanEventLauncher;
 
     @Autowired
     private LoanCalculatorFactory loanCalculatorFactory;
@@ -276,10 +280,25 @@ public class LoanMo9mqListener implements IMqMsgListener{
 		}
 		try {
 			LendOrderEntity lendOrderEntity = lendOrderService.getByOrderId(orderId);
-			if (lendOrderEntity == null || !StatusEnum.LENDING.name().equals(lendOrderEntity.getStatus())) {
+			if (lendOrderEntity == null) {
 				logger.info("接收到了订单[{}]放款的MQ, 然而查不到放款订单, 可能由于放款请求未结束而MQ先到, 延后再次发送MQ", orderId);
 				return MqAction.ReconsumeLater;
 			}
+			if (StatusEnum.SUCCESS.name().equals(lendOrderEntity.getStatus()) || StatusEnum.FAILED.name().equals(lendOrderEntity.getStatus())) {
+                logger.warn("接收到了订单[{}]放款的MQ, 然而查到的放款订单状态为[{}], 可能由于状态机报错而未更新借款订单状态", orderId, lendOrderEntity.getStatus());
+                LoanOrderEntity loanOrderEntity = loanOrderService.getByOrderId(orderId);
+                if (StatusEnum.LENDING.name().equals(loanOrderEntity.getStatus())) {
+                    LoanResponseEvent event = null;
+                    if (isSucceed) {
+                        event =  new LoanResponseEvent(loanOrderEntity.getOrderId(), lendAmount, isSucceed, lendSettleTime, "放款成功", "lendorder[" + lendOrderEntity.getStatus() + "], 再次接到MQ后改为成功");
+                    } else {
+                        event =  new LoanResponseEvent(loanOrderEntity.getOrderId(), BigDecimal.ZERO, isSucceed, lendSettleTime, "放款失败", "lendorder[" + lendOrderEntity.getStatus() + "], 再次接到MQ后改为失败");
+                    }
+                    loanEventLauncher.launch(event);
+                    logger.warn("lendOrder状态[{}], 原loanOrder[{}]状态[{}], 是否成功[{}]", lendOrderEntity.getStatus(), loanOrderEntity.getOrderId(), loanOrderEntity.getStatus(), isSucceed);
+                    return MqAction.CommitMessage;
+                }
+            }
 
 			lendEventLauncher.launch(lendResponse);
 		} catch (Exception e) {
