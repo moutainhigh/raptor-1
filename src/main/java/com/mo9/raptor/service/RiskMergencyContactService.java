4ef911e61ac59ff9c8df3d0cb37bb1902c27fbc5
weiwt <felix@21>
2018-10-08 16:59:44 +0800

    添加规则

---
+++ src/main/java/com/mo9/raptor/service/RiskMergencyContactService.java
@@ -0,0 +1,13 @@
+package com.mo9.raptor.service;
+
+import com.mo9.raptor.entity.RiskMergencyContact;
+
+/**
+ * @author wtwei .
+ * @date 2018/10/8 .
+ * @time 14:54 .
+ */
+public interface RiskMergencyContactService {
+    
+    RiskMergencyContact save(RiskMergencyContact entity);
+}
---
+++ src/main/java/com/mo9/raptor/service/RiskTelYellowPageService.java
@@ -0,0 +1,15 @@
+package com.mo9.raptor.service;
+
+import com.mo9.raptor.entity.RiskTelYellowPage;
+
+/**
+ * @author wtwei .
+ * @date 2018/10/8 .
+ * @time 14:42 .
+ */
+public interface RiskTelYellowPageService {
+    
+    RiskTelYellowPage save(RiskTelYellowPage entity);
+    
+    RiskTelYellowPage update(RiskTelYellowPage updateEntity);
+}
