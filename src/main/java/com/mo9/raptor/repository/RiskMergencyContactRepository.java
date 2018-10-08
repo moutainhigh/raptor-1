f76a39941ad6db0964365f121d2fc5517ba67298
weiwt <felix@21>
2018-10-08 17:10:28 +0800

    add new entity

---
+++ src/main/java/com/mo9/raptor/repository/RiskMergencyContactRepository.java
@@ -0,0 +1,12 @@
+package com.mo9.raptor.repository;
+
+import com.mo9.raptor.entity.RiskMergencyContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author wtwei .
+ * @date 2018/10/8 .
+ * @time 14:44 .
+ */
+public interface RiskMergencyContactRepository extends JpaRepository<RiskMergencyContact, Long> {
+}
