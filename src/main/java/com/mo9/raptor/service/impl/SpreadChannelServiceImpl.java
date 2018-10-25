e3e5dd0873cbe81bb0d3c0dbb5a4694e2ce4bf22
guxt <xtgu@mo9.com>
2018-10-25 11:50:18 +0800

    还款查询是否存在还款中数据

--- src/main/java/com/mo9/raptor/service/impl/SpreadChannelServiceImpl.java
+++ src/main/java/com/mo9/raptor/service/impl/SpreadChannelServiceImpl.java
@@ -3,6 +3,8 @@ package com.mo9.raptor.service.impl;
 import com.mo9.raptor.entity.SpreadChannelEntity;
 import com.mo9.raptor.repository.SpreadChannelRepository;
 import com.mo9.raptor.service.SpreadChannelService;
+import com.mo9.raptor.utils.log.Log;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import java.util.List;
  */
 @Service
 public class SpreadChannelServiceImpl implements SpreadChannelService{
+    private static Logger logger = Log.get();
 
     @Autowired
     private SpreadChannelRepository spreadChannelRepository;
@@ -37,6 +40,7 @@ public class SpreadChannelServiceImpl implements SpreadChannelService{
         }
         for (SpreadChannelEntity entity: list){
             String source1 = entity.getSource();
+            logger.info("支持渠道" + source1 + "进入渠道" + source);
             if(source.equals(source1)){
                 return true;
             }
