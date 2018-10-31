1d5e3c422b5b0d3b4b1c4cde1d47f3679319af3b
guxt <xtgu@mo9.com>
2018-10-31 14:13:37 +0800

    修改定时器查询数据 , 黑名单启动时间 第一次启动确认为15点

--- src/main/java/com/mo9/raptor/task/UserBlackTask.java
+++ src/main/java/com/mo9/raptor/task/UserBlackTask.java
@@ -2,10 +2,12 @@ package com.mo9.raptor.task;
 
 import com.mo9.raptor.engine.entity.LoanOrderEntity;
 import com.mo9.raptor.engine.service.ILoanOrderService;
+import com.mo9.raptor.utils.CommonValues;
 import com.mo9.raptor.utils.RiskUtilsV2;
 import com.mo9.raptor.utils.log.Log;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -29,20 +31,26 @@ public class UserBlackTask {
     @Autowired
     private RiskUtilsV2 riskUtilsV2 ;
 
-    @Scheduled(cron = "0 0/1 * * * ?")
+    @Value("${task.open}")
+    private String taskOpen ;
+
+    @Scheduled(cron = "0 0 15 * * ?")
     public void verifyOverdueOrder(){
-        logger.info("定时器检索处理黑名单用户开始");
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.DAY_OF_YEAR , -7);
-        calendar.set(Calendar.HOUR_OF_DAY , 0);
-        calendar.set(Calendar.MINUTE , 0);
-        calendar.set(Calendar.SECOND , 0);
-        List<LoanOrderEntity> loanOrderEntityList = loanOrderService.listByOverDueOrder(calendar.getTime().getTime());
-        for(LoanOrderEntity loanOrderEntity : loanOrderEntityList){
-            riskUtilsV2.verifyNeedToBlack(loanOrderEntity);
+        if(CommonValues.TRUE.equals(taskOpen)){
+            logger.info("定时器检索处理黑名单用户开始");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.add(Calendar.DAY_OF_YEAR , -7);
+            calendar.set(Calendar.HOUR_OF_DAY , 0);
+            calendar.set(Calendar.MINUTE , 0);
+            calendar.set(Calendar.SECOND , 0);
+            List<LoanOrderEntity> loanOrderEntityList = loanOrderService.listByOverDueOrder(calendar.getTime().getTime());
+            for(LoanOrderEntity loanOrderEntity : loanOrderEntityList){
+                riskUtilsV2.verifyNeedToBlack(loanOrderEntity);
+            }
+            logger.info("定时器检索处理黑名单用户结束  -  总处理 : " + loanOrderEntityList.size());
         }
-        logger.info("定时器检索处理黑名单用户结束  -  总处理 : " + loanOrderEntityList.size());
+
     }
 
 
