0666af11f6ae3b460a88c45e08294e6c603a5413
jyou <jyou@mo9.com>
2018-11-03 15:03:12 +0800

    手动触发上传规则

--- src/main/java/com/mo9/raptor/risk/service/RiskAuditService.java
+++ src/main/java/com/mo9/raptor/risk/service/RiskAuditService.java
@@ -1,6 +1,9 @@
 package com.mo9.raptor.risk.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.mo9.raptor.engine.state.event.impl.AuditResponseEvent;
+
+import java.util.List;
 
 /**
  * @author yngong
@@ -8,4 +11,11 @@ import com.mo9.raptor.engine.state.event.impl.AuditResponseEvent;
 public interface RiskAuditService {
 
     AuditResponseEvent audit(String userCode);
+
+    /**
+     * 手动调用接口时调用，会把结果上传到oss，不做任何记录
+     * @param userCode
+     * @return
+     */
+    JSONObject manualAudit(String userCode);
 }
